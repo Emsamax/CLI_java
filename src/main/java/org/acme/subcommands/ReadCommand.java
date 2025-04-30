0858ebb41e9372a2a9c0d239a027e80a6cb66377
martin <martin.neron@laposte.net>
2025-04-30 17:33:19 +0200

    upgrade code

--- src/main/java/org/acme/subcommands/ReadCommand.java
+++ src/main/java/org/acme/subcommands/ReadCommand.java
@@ -4,6 +4,7 @@ import picocli.CommandLine;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 @CommandLine.Command(name = "read")
 public class ReadCommand implements Runnable {
@@ -11,18 +12,20 @@ public class ReadCommand implements Runnable {
     @CommandLine.Option(names = {"-f", "--file"}, description = "The file to read", required = true)
     String file;
 
+    private String EMPTY_FILE_EXCEPTION = "File is empty";
+
     public void run() {
-        System.out.println("Read command");
-        try{
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = bufferedReader.readLine();
-            while(line != null){
+        //automatically closes the resources within the try block
+        try (var bufferedReader = new BufferedReader(new FileReader(file))) {
+            var line = bufferedReader.readLine();
+            if (line == null) {
+                throw new IOException(EMPTY_FILE_EXCEPTION);
+            }
+            while (line != null) {
                 System.out.println(line);
                 line = bufferedReader.readLine();
-                System.out.println(line);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
